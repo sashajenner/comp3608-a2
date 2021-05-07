5f7a69fef8a2ae9235813f252914ee80ca766bc7
Sasha Jenner <jenner.sasha@gmail.com>
2021-05-07 16:18:51 +1000

    Read files

--- src/MyClassifier.py
+++ src/MyClassifier.py
@@ -6,6 +6,5 @@ train_fname, test_fname, alg = sys.argv[1:4]
 train_f = open(train_fname)
 test_f = open(test_fname)
 
-train_data = [row for row in train_f.readlines()]:
-    line_split = line.split(',')
-    print(line_split)
+train_data = [row.split(',') for row in train_f.readlines()]
+test_data = [row.split(',') for row in test_f.readlines()]
