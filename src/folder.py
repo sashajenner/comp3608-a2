6825a909ff6f3cc8abc29b2ef33b3c363bff26b5
Sasha Jenner <jenner.sasha@gmail.com>
2021-05-09 08:45:44 +1000

    Partially done - creating generator for pima-folds.csv

---
+++ src/folder.py
@@ -0,0 +1,43 @@
+'''
+Split a dataset into 10 folds for stratified cross-validation.
+Number of instances per fold doesn't vary by more than one.
+Ratio of yes and no examples approximately equal for each fold.
+Print out the result.
+
+Usage: python3.8 $0 <DATA_PATH>
+'''
+
+import sys
+import math
+
+data_fname = sys.argv[1]
+data_f = open(data_fname)
+data = [row.rstrip().split(',') for row in data_f.readlines()]
+
+n_folds = 10
+folds = [[] for i in range(n_folds)]
+
+
+n_rows = len(data)
+n_yes = 0
+n_no = 0
+for row in data:
+    if row[-1] == 'yes':
+        n_yes += 1
+    elif row[-1] == 'no':
+        n_no += 1
+
+# Find the number of instances in each fold
+lower = math.floor(n_rows / n_folds)
+upper = math.ceil(n_rows / n_folds)
+n_upper = round((n_rows / n_folds - lower) * n_folds)
+split = [upper if i < n_upper else lower for i in range(n_folds)]
+
+# Find the number of yes in each fold
+lower_y = math.floor(n_yes / n_folds)
+upper_y = math.ceil(n_yes / n_folds)
+n_upper_y = round((n_yes / n_folds - lower_y) * n_folds)
+split_y = [upper_y if i < n_upper_y else lower_y for i in range(n_folds)]
+
+
+data_f.close()
